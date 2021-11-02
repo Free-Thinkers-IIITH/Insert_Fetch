7cc5314d32457e1379ae8788aa7b0e4a1fa790dc
hari <harikn77@gmail.com>
2021-11-02 10:22:02 +0530

    insert_conf.py

--- insert_conf.py
+++ insert_conf.py
@@ -9,7 +9,7 @@ requesting = []
 with open('ranks.json') as f:
     for conf in json.load(f):
         temp = dict()
-        temp["name"] = conf["Standard Name"]
+        temp["name"] = conf["Acronym"] + " " +conf["Standard Name"]
         temp["rank"] = conf["Rank"]
         requesting.append(InsertOne(temp))
 
